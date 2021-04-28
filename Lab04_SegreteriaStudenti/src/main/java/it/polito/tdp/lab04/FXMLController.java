fe41d84f3f08f6cd76b3231bfc62f09648842091
Elisabetta <s261270@studenti.polito.it>
2021-04-28 12:51:15 +0200

    Implementazione punto 1

--- Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
+++ Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
@@ -1,23 +1,100 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.lab04;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import it.polito.tdp.lab04.model.*;
+import it.polito.tdp.lab04.model.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	Model modello;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private ComboBox<Corso> boxCorsi;
+
+    @FXML
+    private Button btnCercaIscritti;
+
+    @FXML
+    private TextField idMatricola;
+
+    @FXML
+    private Button btnGreen;
+
+    @FXML
+    private TextField idNome;
+
+    @FXML
+    private TextField idCognome;
+
+    @FXML
+    private Button btnCercaCorsi;
+
+    @FXML
+    private Button btnIscrivi;
+
+    @FXML
+    private TextArea txtOutput;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void cercaCorsi(ActionEvent event) {
 
     }
+
+    @FXML
+    void cercaIscrittoCorso(ActionEvent event) {
+
+    }
+
+    @FXML
+    void completamentoAutomatico(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert boxCorsi != null : "fx:id=\"boxCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaIscritti != null : "fx:id=\"btnCercaIscritti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert idMatricola != null : "fx:id=\"idMatricola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnGreen != null : "fx:id=\"btnGreen\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert idNome != null : "fx:id=\"idNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert idCognome != null : "fx:id=\"idCognome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaCorsi != null : "fx:id=\"btnCercaCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtOutput != null : "fx:id=\"txtOutput\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        
+    }
+
+	public void setModel(Model m) {
+		this.modello = m;
+		System.out.println(this.modello.getCorsi());
+		this.boxCorsi.getItems().addAll(this.modello.getCorsi());
+	}
+	
 }
--- Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/model/Model.java
+++ Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/model/Model.java
@@ -1,5 +1,19 @@
 package it.polito.tdp.lab04.model;
 
+import java.util.List;
+
+import it.polito.tdp.lab04.DAO.CorsoDAO;
+
 public class Model {
+	
+	CorsoDAO corsi;
+	
+	public Model() {
+		corsi = new CorsoDAO();
+	}
+	
+	public List<Corso> getCorsi(){
+		return this.corsi.getTuttiICorsi();
+	}
 
 }
